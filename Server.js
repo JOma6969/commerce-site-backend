06260d6e67f55b159b23c592bb81357d7e014cbf
Olatunji Lawal <lawaltreasure08@gmail.com>
2025-07-06 20:35:54 +0100

    connect db

--- Server.js
+++ Server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config(); // Load environment variables from .env file
 
 app.get("/", (req, res) => {
   res.send("Hello, World!"); // Basic route to test the server
 });
 
 app.listen(5000, () => {
+  connectDB(); // Connect to the database
   console.log("Server is running on port 5000");
 });
